c277c8c819944a729629078a80898c6f1ecd9f49
vyshnavi_mudraboina <vyshnavimudraboina4@gmail.com>
2026-07-25 13:44:17 +0530

    day 9
    examples solved

--- examples.py
+++ examples.py
@@ -84,28 +84,70 @@ for j in num:
         emt_.append(j)
 print(emt_)
 ---------------------------
-        
-'''
-num = int(input('enter a number: '))
-per_num = 0
-for j in range(1,num):
-    if num % j == 0:
-       per_num += j
-if per_num == num:
-   print(f'{num} is per num')
+print('*', end = '')
+print()
+print('****' ,end ='')
+print()
+print('**',end = '')
+print()
+--------------------------
+fabanocci series
+ex;
+tab_ = int(input('enter a number:' )
+for j in range (1,11):
+    print(f'{tab_} X {j} = {tab_*j}')
+
+output;
+7 X 1 = 7
+7 X 2 = 14
+7 X 3 = 21
+7 X 4 = 28
+7 X 5 = 35
+7 X 6 = 42
+7 X 7 = 49
+7 X 8 = 56
+7 X 9 = 63
+7 X 10 = 70
+------------------
+amstrong number
+ ex;
+num = int(input('enter anumber: '))
+length_ = len(str(num))
+am_ = 0
+for j in str(num):
+    am_ = int(j) ** length_ + am_
+if am_ == num:
+    print(f'{num} is amstrong')
 else:
-   print(f'{num} is not per num')
+    print(f'{num} is not')
+--------------------------------
+fabonocci series
+
+ex:
+limit_ = int(input("ente a number: "))
+num = 0
+num_2 = 1
+print(num,num_2,end=' ')
+for j in range (1,limit_+1):
+    all_ad =  num + num_2
+    num = num_2
+    num_2 = all_ad
+    print(all_ad,end=' ')
+--------------------------------
+calculation
+ex;
+num_1 = int(input("enter a number: "))
+num_2 = int(input("enter a number: "))
+opt_ = int(input("enter \n1.add \n2.sub \n3.* \n4./: "))
+if opt_ == 1:
+    print(num_1 + num_2)
+elif opt_ == 2:
+    print(num_1 - num_2)
 
 
 
-
-
-
-
-
-
-
-
+       
+'''
 
 
 
