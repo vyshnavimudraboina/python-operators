973f1eb680a26208fc5f8155ad301c9e7eebdcab
vyshnavi_mudraboina <vyshnavimudraboina4@gmail.com>
2026-07-27 14:54:12 +0530

    day 10
    functions examples

---
+++ functions.py
@@ -0,0 +1,60 @@
+'''
+functions
+-----------
+-->func is a block that can be executes when we call it
+-->to avoid the repeated lines of code
+syntax:
+def function_name(parameters):
+     -----------
+     -----------
+     -----------
+function_name(arguments)
+----------------------------
+types of functions
+1.bulit-in
+ex;
+i.print()
+ii.len()
+iii.max()
+iv.min()
+
+2.user-define
+==> user-define are the functions that are develop by the user
+ex;
+num = 56
+num_2 = 89
+def total_(num , num_2):
+    print(num + num_2)
+total_(num,num_2)
+total_ (1,2)
+-------------------
+ex;
+required arguments
+
+num = 56
+num_2 = 89
+num_3 = 7
+def total_(num , num_2,num_3):
+    print(num)
+total_(num,num_2,num_3)
+total_(1,2,3)
+------------------------
+positional arguments
+------------------
+==>i t does not matter how we are passing the variable,
+if we assign the value to that variable in the calling
+
+ex;
+def name_(name,name_):
+    print(name)
+    print(name_)
+name_(name = 'vyshnavi',name_ ='mudraboina')
+
+
+
+'''
+def pos_(a,b,c,d,e):
+    print(a)
+pos_(a=0,b=3,c=5,d=8,e=9)
+
+
