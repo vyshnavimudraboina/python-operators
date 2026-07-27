75f9d9b800ce19e03f6c4102e64c9df620d5efd6
vyshnavi_mudraboina <vyshnavimudraboina4@gmail.com>
2026-07-27 15:02:52 +0530

    day 8
    output formating

---
+++ output formating.py
@@ -0,0 +1,49 @@
+'''
+output formating
+-------------------
+name = 'vyshu'
+age = 67
+print('welcome',name,'your age is',age)
+2.f-string(doc-string)
+----------------------------
+name = 'vyshu'
+age = 67
+print(f'welcome {name} your age is {age}')
+name = 'vyshu'
+age = 67
+print('name: %s' % name)
+
+
+age = 34
+print('name: %s' % name)
+
+%s---> all
+name= 'vyshu'
+print('name: %s' % name)
+
+name = 'vyshu'
+age = 67
+print('name: %s' % name)
+name = 'vyshu'
+price = 67
+print('name: %f' % price)
+-------------
+%d--->digit
+
+price = 67
+print('name: %d' % price)
+
+%f--->float
+
+name = 'vyshu'
+price = 67
+print('name: %f' % price)
+
+ line by line output
+ ------------------
+ name = 'vyshu'
+price = 67
+print('name: {} \nprice : {}'.format(name,price))
+
+ 
+'''
